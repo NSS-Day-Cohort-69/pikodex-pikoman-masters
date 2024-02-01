4e2060d197ff484be0ce470fa9bf59067f0ad649
Zavier Hopson <zo.hopson94@gmail.com>
2024-02-01 11:26:02 -0600

    part 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-const container = document.querySelector('#container')
+const container = document.querySelector("#container");
+
+//create html skeleton "JAYME"
+//>>>9 pikomon information packs
+//>>>only 1 class needed for all images
+//>>>either 3 sections or flex-flow: column wrap
+//add flesh (details)"WILL"
+//>>>1 class needed for information
+//implement css elements for arrangement "ZAVIER"
+//>background color:#f7e625
+//>column background color: #d4a3f7
+//>font-family: Honk; arial (for header)
+//>column-borders: black, dashed
+//>img-borders: solid black
+//>info-border: none
+//>>underline/bold the name of each pikomon
+//>regular arial for info font
